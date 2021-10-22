853cf94c184946977f79ab1d6d8c3a865ff8a44f
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-10-22 15:42:29 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = nombrealeatorio();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let nombrealeatorio = () => {
+  let quien = ["Mi mamá", "Mi perro", "El vecino", "El gato", "El vendedor"];
+  let accion = [
+    "se cayó sobre",
+    "tuvo un accidente en",
+    "se desmayo en",
+    "se fue hacia",
+    "salió corriendo hacia"
+  ];
+  let cuando = [
+    "el baño",
+    "el segundo piso",
+    "la bencinera",
+    "la cocina",
+    "el supermercado"
+  ];
+
+  let quienIndx = Math.floor(Math.random() * quien.length);
+  let accionIndx = Math.floor(Math.random() * accion.length);
+  let cuandoIndx = Math.floor(Math.random() * cuando.length);
+
+  return quien[quienIndx] + " " + accion[accionIndx] + " " + cuando[cuandoIndx];
 };
